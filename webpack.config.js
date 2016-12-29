9dd14376ba6447959330ac5cb7eed7d0d3b5775f
Viktor Gustavsson <villor94@gmail.com>
2016-12-29 02:16:42 +0100

    Renamed client -> app

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var webpack = require('webpack');
 var path = require('path');
 
 var BUILD_DIR = path.resolve(__dirname, 'public');
-var APP_DIR = path.resolve(__dirname, 'client');
+var APP_DIR = path.resolve(__dirname, 'app');
 
 var config = {
 	entry: APP_DIR + '/index.jsx',
